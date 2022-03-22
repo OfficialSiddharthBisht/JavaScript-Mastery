cd4db9bc6d8b49a0cc77c452e0cb0d5be3f60312
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-22 10:14:13 +0530

    learned coercion in javascript

---
+++ JavaScript Concept/coercion.js
@@ -0,0 +1,15 @@
+/*
+* Falsy Values
+undefined
+null
+0
+''
+Nan 
+* Truthy Values
+Anything Other then these.
+*/
+
+console.log("2" + 2); // output 22
+
+// ==  Dosent deal with data type 
+// ===  Deals with data type
