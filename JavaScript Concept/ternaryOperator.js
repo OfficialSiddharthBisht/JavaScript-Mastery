5814c2cf18581bc2e7e7f8c30ccc6f98432a4eeb
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-22 10:12:48 +0530

    revised ternary operator

---
+++ JavaScript Concept/ternaryOperator.js
@@ -0,0 +1,12 @@
+let isPresent = true;
+
+if(isPresent){
+    console.log("Mark Present");
+}else{
+    console.log("Mark Absent");
+}
+
+
+//* it is same as this
+isPresent ? console.log("Mark Present") : console.log("Mark Absent"); 
+// Ternary Operator is just a syntaxical sugar of if else rest all the working is same
