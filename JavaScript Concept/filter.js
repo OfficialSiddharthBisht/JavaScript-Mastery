313ca5fe793582eef3ffa84e8105fa6dedd0327b
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-23 11:18:01 +0530

    filter method

---
+++ JavaScript Concept/filter.js
@@ -0,0 +1,4 @@
+// * Filter method basically filters out all the elements according to the condition that we provide.
+let arr = [ 10, 20, 30, 40, 50];
+let newArr = arr.filter(N => (N > 30));
+console.log(newArr);
