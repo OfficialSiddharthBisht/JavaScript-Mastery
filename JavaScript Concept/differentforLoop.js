651488fcfe8b5d1ea73e507f89856aef3856ef68
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-23 11:17:01 +0530

    coded the for loop in a different way

---
+++ JavaScript Concept/differentforLoop.js
@@ -0,0 +1,9 @@
+// * This is one more way in which we can write for loop just to know and this is not how it is written for production level applications
+let i = 0;
+for(;;){
+    if(i > 3){
+        break;
+    } 
+    console.log(i);
+    i++
+}
