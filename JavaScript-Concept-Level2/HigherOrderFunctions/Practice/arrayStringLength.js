659ce27f8000bc18da42a57e52fab66793749899
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-30 13:52:59 +0530

    solved array string length problem

---
+++ JavaScript-Concept-Level2/HigherOrderFunctions/Practice/arrayStringLength.js
@@ -0,0 +1,10 @@
+/*
+Given an array of strings print the length of each string
+
+example : ["apple", "windows", "ubuntu"]
+*/
+
+const input = ["apple","windows","ubantu"];
+
+const output = input.map((i)=>i.length);
+console.log(output);
